285e2594b36b52ea7893f5f0c970bbe234fc5b8b
AndiB <sudofortune@gmail.com>
2015-07-28 20:36:18 -0700

    made changes to satisfy pep8, but somehow that made it 0.05 points worse on pylint, either way, only thing left to do is add proper doc strings

--- src/python/ATPC.py
+++ src/python/ATPC.py
@@ -1,6 +1,7 @@
 import tables
 import logging
 
+
 def pet_to_asc_c(in_char):
     in_char = chr(in_char)
     logging.info("in petToPetC")
@@ -12,6 +13,7 @@ def pet_to_asc_c(in_char):
     ret = tables.petToAscTable[ord(in_char)]
     logging.info("pet character: " + str(ord(in_char)) + " to " + str(chr(ret)))
     print("the pet character: " + str(ord(in_char)) + " to " + str(chr(ret)))
+
 
 def pet_to_asc_s(in_str):
     # not sure if you need this or what you'd pass in
@@ -29,6 +31,7 @@ def asc_to_pet_c(in_char):
     logging.info("ascii character: " + str(in_char) + " to " + str(ret))
     print("ascii character: " + str(in_char) + " to " + str(ret))
     return ret
+
 
 def asc_to_pet_s(in_str):
     logging.info("in asc_to_pet_s")
@@ -49,4 +52,4 @@ def examples():
     pet_to_asc_c(193)
     pet_to_asc_s('hi there')
 
-#examples()
+# examples()

f2f30658d92e6e7b186407f494aba69891804f09
AndiB <sudofortune@gmail.com>
2015-07-28 20:55:19 -0700

    added doc strings and ran it through pep8 and pylint again to get rid of most complaints, using python 3 print convention for now, this should work on everyone's python 2

--- src/python/ATPC.py
+++ src/python/ATPC.py
@@ -1,26 +1,46 @@
+"""
+Purpose: Convert single character of ASCII to PETSCII and vice versa.
+         Also converts string of ASCII to list of hex PETSCII values.
+"""
+
 import tables
 import logging
 
 
 def pet_to_asc_c(in_char):
+    """
+    Purpose: converts PETSCII character to ASCII
+    Input: single character/hex value
+    Output: returns hex value of ASCII character
+    """
     in_char = chr(in_char)
     logging.info("in petToPetC")
     if len(in_char) != 1:
-        logging.warning("in petToPetC")
+        logging.warning("in pet_to_asc_c")
         logging.warning("you are calling the wrong function")
         logging.warning("parameter should be a single character")
         return -1
     ret = tables.petToAscTable[ord(in_char)]
     logging.info("pet character: " + str(ord(in_char)) + " to " + str(chr(ret)))
     print("the pet character: " + str(ord(in_char)) + " to " + str(chr(ret)))
+    return ret
 
 
 def pet_to_asc_s(in_str):
+    """
+    Purpose: converts PETSCII string to ASCII
+    TODO: clearly this is not implemented yet
+    """
     # not sure if you need this or what you'd pass in
     logging.info("in pet_to_asc_s "+str(in_str))
 
 
 def asc_to_pet_c(in_char):
+    """
+    Purpose: converts ASCII character to PETSCII
+    Input: single character/hex value
+    Output: returns hex value of PETSCII character
+    """
     logging.info("in asc_to_pet_c")
     if len(in_char) != 1:
         logging.warning("in asc_to_pet_c")
@@ -34,6 +54,11 @@ def asc_to_pet_c(in_char):
 
 
 def asc_to_pet_s(in_str):
+    """
+    Purpose: converts ASCII string to list of PETSCII hex values
+    Input: ASCII string
+    Output: returns list of hex values of PETSCII characters
+    """
     logging.info("in asc_to_pet_s")
     ret = []
     for in_char in in_str:
@@ -43,13 +68,18 @@ def asc_to_pet_s(in_str):
 
 
 def examples():
-    asc_to_pet_c('c')
-    asc_to_pet_c('A')
+    """
+    Purpose: runs through some simple examples
+    Input: nothing
+    Output: Side effect is that things are printed to the screen
+    """
+    print(asc_to_pet_c('c'))
+    print(asc_to_pet_c('A'))
     print("")
-    asc_to_pet_s("hi there A")
+    print(asc_to_pet_s("hi there A"))
     print ("")
-    pet_to_asc_c(67)
-    pet_to_asc_c(193)
-    pet_to_asc_s('hi there')
+    print(chr(pet_to_asc_c(67)))
+    print(chr(pet_to_asc_c(193)))
+    print(pet_to_asc_s('hi there'))
 
 # examples()
